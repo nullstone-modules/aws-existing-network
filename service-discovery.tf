8ca41be2ad9b9e3d1c99da7587ec7c26b706b235
Brad Sickles <brad.sickles@gmail.com>
2021-05-07 15:15:53 -0400

    Added private dns namespace creation

---
+++ service-discovery.tf
@@ -0,0 +1,5 @@
+resource "aws_service_discovery_private_dns_namespace" "service" {
+  name = var.service_namespace
+  vpc  = data.aws_vpc.this.id
+  tags = data.ns_workspace.this.tags
+}
--- variables.tf
+++ variables.tf
@@ -1,12 +1,20 @@
 variable "id" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
   description = <<EOF
 The ID of the VPC to match.
 EOF
 }
 
+variable "service_namespace" {
+  type        = string
+  description = <<EOF
+Private DNS Domain to register in the network that is used to attach dynamically allocated services. (e.g. containers)
+EOF
+  default     = "service"
+}
+
 locals {
-  id_filter   = var.id == "" ? {} : map("vpc-id", var.id)
-  filters     = merge(local.id_filter)
+  id_filter = var.id == "" ? {} : map("vpc-id", var.id)
+  filters   = merge(local.id_filter)
 }
