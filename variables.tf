a7fb744eb341406e4d3872aaa592f69480938eed
Brad Sickles <brad.sickles@gmail.com>
2021-05-07 14:32:28 -0400

    Removing name variable

--- variables.tf
+++ variables.tf
@@ -1,23 +1,12 @@
-variable "name" {
-  type    = string
-  description = <<EOF
-The Name of the VPC to match.
-At least one filter is required: `id`, `name`.
-Specifying both is not desired; the module will attempt to filter on both values.
-EOF
-}
-
 variable "id" {
   type    = string
+  default = ""
   description = <<EOF
 The ID of the VPC to match.
-At least one filter is required: `id`, `name`.
-Specifying both is not desired; the module will attempt to filter on both values.
 EOF
 }
 
 locals {
-  name_filter = var.name == "" ? {} : map("tag:Name", var.name)
   id_filter   = var.id == "" ? {} : map("vpc-id", var.id)
-  filters     = merge(local.name_filter, local.id_filter)
+  filters     = merge(local.id_filter)
 }
