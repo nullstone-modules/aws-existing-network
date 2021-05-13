29959228bd358d6a600b7c00d6897942e153ae3c
Brad Sickles <brad.sickles@gmail.com>
2021-05-13 18:11:22 -0400

    Fixed

--- dns-internal.tf
+++ dns-internal.tf
@@ -3,7 +3,7 @@ resource "aws_route53_zone" "internal" {
   comment = "Supports internal DNS discovery of services"
 
   vpc {
-    vpc_id = module.network.vpc_id
+    vpc_id = data.aws_vpc.this.id
   }
 
   tags = data.ns_workspace.this.tags
